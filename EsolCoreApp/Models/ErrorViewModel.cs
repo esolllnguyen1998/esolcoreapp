fba280b93ec595ec39226740c8213e02be6c2e52
Thang Nguyen Quoc <thang.nguyen@orientsoftware.com>
2019-08-15 09:28:19 +0700

    Add project files.

---
+++ EsolCoreApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EsolCoreApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
